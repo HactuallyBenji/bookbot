3bec791ed3093f20ab9247b66503e5d4088f8300
Hactually Benji <mrbenamiller@gmail.com>
2024-02-26 17:05:38 -0800

    Interested in character frequency

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ def main():
     word_count = get_word_count(file_contents)
     print(f"The text contains {word_count} words.")
 
+    character_counts = get_character_counts(file_contents)
+    print(f"The text has many characters. Here's are the frequencies of each.")
+    print(character_counts)
+
 def get_text(file_path):
     with open(file_path) as file:
         return file.read()
 
 def get_word_count(text):
     return len(text.split())
+
+def get_character_counts(text):
+    character_counts = {}
+    for character in text:
+        if character in character_counts:
+            character_counts[character] += 1
+        else:
+            character_counts[character] = 1
+    return character_counts
 
 if __name__ == "__main__":
     main()
