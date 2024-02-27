25d6dee342b5286924acc530646f14900c9d8ceb
Hactually Benji <mrbenamiller@gmail.com>
2024-02-26 17:03:29 -0800

    Pull out into maintainable function with easy file change

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main():
-    with open("./books/frankenstein.txt") as file:
-        file_contents = file.read()
-        print(file_contents)
+    file_path = "./books/frankenstein.txt"
+    file_contents = get_text(file_path)
+    word_count = get_word_count(file_contents)
+    print(file_contents)
+    print(word_count)
 
-        words = file_contents.split()
-        print(len(words))
+def get_text(file_path):
+    with open(file_path) as file:
+        return file.read()
+
+def get_word_count(text):
+    return len(text.split())
 
 if __name__ == "__main__":
     main()
